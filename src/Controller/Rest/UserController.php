4311ed6c7b8e7d9911702b9f8652137ce9073f96
SAUVAGE <emmanuel.sauvage@live>
2019-11-27 10:00:42 +0100

    Create controller

--- src/Controller/Rest/UserController.php
+++ src/Controller/Rest/UserController.php
@@ -4,8 +4,11 @@ namespace App\Controller\Rest;
 
 use App\Entity\User;
 use App\Exception\ResourceValidationException;
+use App\Repository\UserRepository;
+use App\Representation\Users;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Request\ParamFetcherInterface;
 
 class UserController extends AbstractFOSRestController
 {
@@ -31,5 +34,53 @@ class UserController extends AbstractFOSRestController
         }
 
         return $user;
+    }
+
+    /**
+     * @Rest\Get("/users", name="app_user_list")
+     *
+     * @Rest\QueryParam(
+     *     name="wordsearch",
+     *     requirements="[a-zA-Z0-9]*",
+     *     nullable=true,
+     *     description="The keyword to search for."
+     * )
+     * @Rest\QueryParam(
+     *     name="order",
+     *     requirements="asc|desc",
+     *     default="asc",
+     *     description="Sort order (asc or desc)"
+     * )
+     * @Rest\QueryParam(
+     *     name="limit",
+     *     requirements="\d+",
+     *     default="10",
+     *     description="Max number of articles per page."
+     * )
+     * @Rest\QueryParam(
+     *     name="offset",
+     *     requirements="\d+",
+     *     default="1",
+     *     description="The pagination offset"
+     * )
+     *
+     * @Rest\View
+     * @param UserRepository $userRepository
+     * @param ParamFetcherInterface $paramFetcher
+     * @return Users
+     *
+     */
+    public function listAction(ParamFetcherInterface $paramFetcher,UserRepository $userRepository)
+    {
+        $client = $this->getUser();
+        $pager = $userRepository->search(
+            $client->getId(),
+            $paramFetcher->get('wordsearch'),
+            $paramFetcher->get('order'),
+            $paramFetcher->get('limit'),
+            $paramFetcher->get('offset')
+        );
+
+        return new Users($pager,$paramFetcher->get('wordsearch'));
     }
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -3,7 +3,6 @@
 namespace App\Repository;
 
 use App\Entity\User;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
@@ -12,10 +11,30 @@ use Doctrine\Common\Persistence\ManagerRegistry;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends AbstractRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
+    }
+
+    public function search($client_id, $wordsearch, $order = 'asc', $limit = 20,$offset=1)
+    {
+        $qb = $this
+            ->createQueryBuilder('u')
+            ->select('u')
+            ->Where('u.client = :id')
+            ->setParameter('id', $client_id)
+            ->orderBy('u.username', $order)
+        ;
+
+        if ($wordsearch) {
+            $qb
+                ->andWhere('u.username LIKE ?1')
+                ->setParameter(1, '%'.$wordsearch.'%')
+            ;
+        }
+
+        return $this->paginate($qb, $limit,$offset);
     }
 }
