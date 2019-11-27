a39f9d64eb1b499c1e96f1236fc15943b4678696
SAUVAGE <emmanuel.sauvage@live>
2019-11-27 11:23:40 +0100

    Optimise code

--- src/Controller/Rest/UserController.php
+++ src/Controller/Rest/UserController.php
@@ -7,10 +7,13 @@ use App\Exception\ResourceValidationException;
 use App\Repository\UserRepository;
 use App\Representation\Users;
 use App\Security\UserVoter;
+use App\Service\UserManager;
+use App\Validator\UserValidator;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcherInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractFOSRestController
 {
@@ -95,27 +98,52 @@ class UserController extends AbstractFOSRestController
      *     requirements = {"id"="\d+"}
      * )
      * @Rest\View( StatusCode = 204)
-     * @param int $id
+     * @param User $user
      * @param UserRepository $repo
-     * @param EntityManagerInterface $manager
+     * @param UserManager $manager
      * @return string
      * @throws ResourceValidationException
      *
      *
      */
-    public function deleteAction(int $id,  UserRepository $repo,  EntityManagerInterface $manager)
+    public function deleteAction(User $user,  UserRepository $repo,  UserManager $manager)
     {
-        $user=$repo->findOneBy(['id'=>$id]);
-
-
-        if (!$user) {
-            $message = 'No users match the number ' . $id;
-            throw new ResourceValidationException($message);
-        }
-
         $this->denyAccessUnlessGranted(UserVoter::DELETE, $user);
 
         $manager->remove($user);
-        $manager->flush();
+    }
+
+    /**
+     * @Rest\Put(
+     *     path = "/users/{id}",
+     *     name = "api_user_modify",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @Rest\View( StatusCode = 200)
+     * @param User $user
+     * @param UserManager $manager
+     * @param Request $request
+     * @return User
+     * @throws ResourceValidationException
+     *
+     */
+    public function modifyAction(User $user, UserManager $manager, Request $request)
+    {
+        $this->denyAccessUnlessGranted(UserVoter::UPDATE, $user);
+
+        //Utilisation de request permettant l'envoi d'un json partiel (username ou email)
+        //tout en restreignant les champs modifiables
+        $request->get('username') &&
+        $user->setUsername($request->get('username')) ;
+
+        $request->get('email') &&
+        $user->setEmail($request->get('email')) ;
+
+        if(!$manager->update($user)  ) {
+            $message = 'The JSON sent contains invalid data. ' . $manager->getErrors($user);
+            throw new ResourceValidationException($message);
+        }
+
+        return $user;
     }
 }
